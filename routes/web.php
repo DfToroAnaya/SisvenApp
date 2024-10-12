594315271343f0c59f74ce8272c8d5aa741c6db6
Diego Toro <dtoroanaya16@gmail.com>
2024-10-12 10:01:03 -0500

    Creacion de Modelo

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+
 Route::get('/categorias', [CategoriaController::class, 'index'])->name('categorias.index');
 Route::post('/categorias', [CategoriaController::class, 'store'])->name('categorias.store');
 Route::get('/categorias/create', [CategoriaController::class, 'create'])->name('categorias.create');
@@ -16,11 +25,6 @@ Route::put('/categorias/{categoria}', [CategoriaController::class, 'update'])->n
 Route::get('/categorias/{categoria}/edit', [CategoriaController::class, 'edit'])->name('categorias.edit');
 
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
